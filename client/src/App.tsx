619f13703ff842fb0ed915381f4983f1d5b5cdfa
Anton Cristo <antoncristo92@gmail.com>
2023-07-23 14:27:24 +0300

    layout and routes infra

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,22 @@
+import { Layout } from './layout';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { routes } from './router';
+
 import './App.css';
 
-export const App = () => <div>Hello Client!</div>;
+export const App = () => (
+	<Layout>
+		<BrowserRouter>
+			<Routes>
+				{routes.map(route => (
+					<Route
+						key={route.path}
+						path={route.path}
+						index={route.index}
+						element={route.component}
+					/>
+				))}
+			</Routes>
+		</BrowserRouter>
+	</Layout>
+);
---
+++ client/src/router/routes.type.ts
@@ -0,0 +1,5 @@
+export interface IRoute {
+	path: string;
+	index: boolean;
+	component: JSX.Element;
+}
